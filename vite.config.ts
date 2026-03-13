2493eaf02f7c91d1fbd78fd2a5bf19b23ff89577
Remy.Ren <remysince2014@hotmail.com>
2026-03-13 19:35:14 +0800

    更新vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
+import react from '@vitejs/plugin-react'
+import {inspectAttr} from 'kimi-plugin-inspect-react'
+import path from 'path'
+import {defineConfig} from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-web-app/',
+  base: '/zxb/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
